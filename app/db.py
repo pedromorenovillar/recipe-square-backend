139d295d8d370059c68f31ee853c37dcbc5bf0e7
Pedro José Moreno Villar <151030573+pedromorenovillar@users.noreply.github.com>
2024-09-15 10:44:32 +0200

    changed db and added duplicate username/email check in routes.py

--- app/db.py
+++ app/db.py
@@ -7,7 +7,7 @@ def init_db(app):
   global db
   MONGODB_URI = os.getenv('MONGODB_URI')
   client = MongoClient(MONGODB_URI)
-  db = client.get_database("testDB")
+  db = client.get_database("recipe_square")
   # TODO change DB name when final DB is implemented
 
 def get_db():
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from .db import get_db
-from pymongo.errors import DuplicateKeyError, PyMongoError
+from pymongo.errors import PyMongoError
 import bcrypt
 
 user_routes = Blueprint('user_routes', __name__)
@@ -14,8 +14,18 @@ def register_user():
     print("Received data:", user_data)
     hashed_password = bcrypt.hashpw(user_data["password"].encode('utf-8'), bcrypt.gensalt())
 
+    user_data["username"] = user_data["username"].lower()
+    user_data["email"] = user_data["email"].lower()
+
     db = get_db()
     users = db.users
+
+    # Check if the username or email already exists in the database || TODO remove print log
+    existing_user = users.find_one({"$or": [{"username": user_data["username"]}, {"email": user_data["email"]}]})
+    print(f"Existing user found: {existing_user}")
+    
+    if existing_user:
+      return jsonify({"error": "Username or email already exists"}), 400
 
     # Insert the user into MongoDB
     new_user = {
@@ -25,13 +35,11 @@ def register_user():
     }
 
     result = users.insert_one(new_user)
+    # TODO remove logs
     print(f"Inserted user with _id: {result.inserted_id}")
 
     return jsonify({f"message": "User registered successfully", "id": str(result.inserted_id)}), 201
   
-  except DuplicateKeyError:
-    return jsonify({"error": "Username or Email already exists"}), 400
-
   except PyMongoError as e:
     print(f"Error registering user into MongoDB: {e}")
     return jsonify({"error": "Database registration failed"}), 500
