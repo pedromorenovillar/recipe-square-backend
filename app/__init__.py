545c94cf9baac9da7d7fff83760641b2300f903d
Pedro José Moreno Villar <151030573+pedromorenovillar@users.noreply.github.com>
2024-10-09 15:49:41 +0200

    added dynamic CORS configuration for development and production environments

--- app/__init__.py
+++ app/__init__.py
@@ -16,8 +16,13 @@ def create_app():
     mail.init_app(app)
     Session(app)
     
-    # Initializes CORS to connect to frontend || TO DO change origins when frontend is deployed.
-    CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": ["http://localhost:8000"]}})
+    # CORS configuration
+    if app.config['ENV'] == 'production':
+        # Allows requests from the deployed frontend in production
+        CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": ["https://recipe-square-frontend-5c021339c6db.herokuapp.com"]}})
+    else:
+        # Allows requests from localhost in development
+        CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": ["http://localhost:5173"]}})
     
 
     # Initializes MongoDB
